a849293348008c99620059c2df91cf7f3afdb41f
Banee Ishaque K <baneeishaque@gmail.com>
2023-02-03 16:37:20 +0530

    In

---
+++ bin/account_ledger_library_dart.dart
@@ -0,0 +1,6 @@
+import 'package:account_ledger_library_dart/account_ledger_library_dart.dart'
+    as account_ledger_library_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${account_ledger_library_dart.calculate()}!');
+}
---
+++ test/account_ledger_library_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:account_ledger_library_dart/account_ledger_library_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
