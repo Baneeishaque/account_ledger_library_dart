d6b3d49af775dca4848179167b6010b0bf05f6dc
Banee Ishaque K <baneeishaque@gmail.com>
2025-11-15 13:26:19 +0530

    fix: update JAVA_HOME environment variable handling for platform-specific configurations
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- lib/utils/account_ledger_kotlin_cli_utils.dart
+++ lib/utils/account_ledger_kotlin_cli_utils.dart
@@ -17,11 +17,24 @@ Tuple2<bool, String> runAccountLedgerKotlinCliOperation(
   ProcessResult processResult = Process.runSync(
     accountLedgerCliExecutable,
     accountLedgerKotlinCliArguments,
-    environment: (platform.isWindows && env.isDefined('JAVA_HOME'))
+    environment: (platform.isWindows && env.isDefined('JAVA_HOME_WINDOWS'))
         ? {
-            "JAVA_HOME": env['JAVA_HOME']!,
+            "JAVA_HOME": env['JAVA_HOME_WINDOWS']!,
           }
-        : {},
+        : (
+          (platform.isMacOS && env.isDefined('JAVA_HOME_MAC'))
+          ? {
+              "JAVA_HOME": env['JAVA_HOME_MAC']!,
+          }
+          : (
+            (platform.isLinux && env.isDefined('JAVA_HOME_LINUX'))
+            ? {
+                "JAVA_HOME": env['JAVA_HOME_LINUX']!,
+            }
+            :
+            {}
+            )
+          ),
     workingDirectory: File(accountLedgerCliExecutable).parent.path,
   );
   if (processResult.exitCode == 0) {
